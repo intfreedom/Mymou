394fbc69761f13748e7ee83b9f75802d56fc63c4
intfreedom <36918526+intfreedom@users.noreply.github.com>
2020-04-10 10:42:15 +0800

    almost

--- Behavioural Testing Unit (Android)/Mymou/app/src/main/java/mymou/task/individual_tasks/TaskTransitiveInference.java
+++ Behavioural Testing Unit (Android)/Mymou/app/src/main/java/mymou/task/individual_tasks/TaskTransitiveInference.java
@@ -42,6 +42,9 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
     private static double score = 0.0;
     private static PreferencesManager prefManager;
     private String start_sequence= "A";
+    private static int right_picture_number = 0;
+    private static int wrong_picture_number = 0;
+    private static int last_n = 11;
 
     /**
      * Function called when task first loaded (before the UI is loaded)
@@ -81,15 +84,15 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
         Random r= new Random();
         int choice = r.nextInt(2);
         if (choice==1){
-            cues[0].setX(150);//Monkey O Cue1
-            cues[1].setX(500);//Monkey O Cue2
-            cues[0].setY(500);//Monkey O Cue1
-            cues[1].setY(500);//Monkey O Cue2
+            cues[0].setX(200);//Monkey O Cue1
+            cues[1].setX(600);//Monkey O Cue2
+            cues[0].setY(700);//Monkey O Cue1
+            cues[1].setY(700);//Monkey O Cue2
         }else {
-            cues[1].setX(150);
-            cues[0].setX(500);
-            cues[1].setY(500);
-            cues[0].setY(500);
+            cues[1].setX(200);
+            cues[0].setX(600);
+            cues[1].setY(700);
+            cues[0].setY(700);
         }
 
     }
@@ -111,7 +114,10 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
         }
 
         Random random = new Random();
-        int n = random.nextInt(10); //随机选择10个图片中的一个；
+        int n; //随机选择10个图片中的一个，相邻两次不重复；public int nextInt(int n), value is [0, n)
+        do{
+            n = random.nextInt(10);
+        }while (n==last_n);
 
         if(prefManager.start_sequence<10){//默认start_sequence=1,开始为A（如果有需要taskseting中设置一个开始的训练集），即1-10,
             start_sequence = "A";
@@ -221,7 +227,7 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
         };
 
         int[][] image_array = {imageListMapA,imageListMapB,imageListMapC,imageListMapD,imageListMapE,imageListMapF,imageListMapG};
-        int n_choice = random.nextInt(6);
+        int n_choice = random.nextInt(6);   //[0, 6), i.e.;
 
         switch (start_sequence) {
             // If they pressed the correct cue, then set the bool to true
@@ -230,6 +236,8 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
 
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapB[n]);
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapC[n]);
+                    right_picture_number = 11+n;
+                    wrong_picture_number = 21+n;
                     start_sequence = "B";
                     score = 0.0;
                     right_choice = 0;
@@ -237,12 +245,16 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
                 }else {//等于所选值；
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapA[n]);
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapB[n]);
+                    right_picture_number = 1+n;
+                    wrong_picture_number = 11+n;
                 }
                 break;
             case "B":
                 if(score>0.9){
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapC[n]);//等于下一个序列；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapD[n]);//Monkey O Cue2
+                    right_picture_number = 21+n;
+                    wrong_picture_number = 31+n;
                     start_sequence = "C";
                     score = 0.0;
                     right_choice = 0;
@@ -250,12 +262,16 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
                 }else {
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapB[n]);//等于所选值；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapC[n]);//Monkey O Cue2
+                    right_picture_number = 11+n;
+                    wrong_picture_number = 21+n;
                 }
                 break;
             case "C":
                 if(score>0.9){
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapD[n]);//等于下一个序列；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapE[n]);//Monkey O Cue2
+                    right_picture_number = 31+n;
+                    wrong_picture_number = 41+n;
                     start_sequence = "D";
                     score = 0.0;
                     right_choice = 0;
@@ -263,12 +279,16 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
                 }else {
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapC[n]);//等于所选值；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapD[n]);//Monkey O Cue2
+                    right_picture_number = 21+n;
+                    wrong_picture_number = 31+n;
                 }
                 break;
             case "D":
                 if(score>0.9){
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapE[n]);//等于下一个序列；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapF[n]);//Monkey O Cue2
+                    right_picture_number = 41+n;
+                    wrong_picture_number = 51+n;
                     start_sequence = "E";
                     score = 0.0;
                     right_choice = 0;
@@ -276,12 +296,16 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
                 }else {
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapD[n]);//等于所选值；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapE[n]);//Monkey O Cue2
+                    right_picture_number = 31+n;
+                    wrong_picture_number = 41+n;
                 }
                 break;
             case "E":
                 if(score>0.9){
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapF[n]);//等于下一个序列；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapG[n]);//Monkey O Cue2
+                    right_picture_number = 51+n;
+                    wrong_picture_number = 61+n;
                     start_sequence = "F";
                     score = 0.0;
                     right_choice = 0;
@@ -289,15 +313,21 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
                 }else {
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapE[n]);//等于所选值；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapF[n]);//Monkey O Cue2
+                    right_picture_number = 41+n;
+                    wrong_picture_number = 51+n;
                 }
                 break;
             default:
                 if(score>0.9){
                     cues_all[monkey_o][0].setBackgroundResource(image_array[n_choice][n]);//等于所选值；
                     cues_all[monkey_o][1].setBackgroundResource(image_array[n_choice+1][n]);
+                    right_picture_number = n_choice*10+1+n;
+                    wrong_picture_number = (n_choice+1)*10+1+n;
                 }else {
                     cues_all[monkey_o][0].setBackgroundResource(imageListMapF[n]);//等于所选值；
                     cues_all[monkey_o][1].setBackgroundResource(imageListMapG[n]);//Monkey O Cue2
+                    right_picture_number = 51+n;
+                    wrong_picture_number = 61+n;
                 }
                 break;
         }
@@ -321,21 +351,26 @@ public class TaskTransitiveInference extends Task implements View.OnClickListene
         // Reset timer for idle timeout on each press
         callback.resetTimer_();
 
-        // Log screen press
-        logEvent(""+view.getId()+" button pressed", callback);
-
         // Now decide what to do based on what cue pressed
         boolean successfulTrial = false;
-        switch (view.getId()) {
+        int picture_number_pressed = wrong_picture_number;
+        String answer_results = "Wrong";
+        switch (view.getId()) {//图片的ID????
             // If they pressed the correct cue, then set the bool to true
             case R.id.buttonCue1MonkO:
                 successfulTrial = true;
                 right_choice += 1;
+                picture_number_pressed = right_picture_number;
+                answer_results = "Right";
                 break;
             case R.id.buttonCue2MonkV:
                 successfulTrial = true;
                 break;
         }
+
+        String note = "PictureNumber:"+picture_number_pressed+", ("+right_picture_number+","+wrong_picture_number+")-"+answer_results;
+        // Log screen press
+        logEvent(""+note, callback);
 
         // Tell parent (TrialManager.java) the outcome of the trial, which will then respond accordingly
         // (e.g. give reward if correct, set up for next trial etc)
